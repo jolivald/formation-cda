34a39b5591574d2ee3d03beeaa064f9ae4bb9864
jolivald <jolivald.jonathan@gmail.com>
2023-10-09 15:45:04 +0200

    start exo-02

---
+++ exo-02/exercice-01.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Exercice 1
+
+Ecrire un script PHP qui affiche tous les nombres impairs entre 0 et 150, par ordre
+croissant : 1 3 5 7...
+*/
+
+for ($i=0; $i<151; $i+=2){
+    echo $i . "\n";
+}
