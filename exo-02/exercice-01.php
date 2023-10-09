3af95853902bcd4bd9e8ad3c9e0d64503c200366
jolivald <jolivald.jonathan@gmail.com>
2023-10-09 15:49:09 +0200

    switch even to odd

--- exo-02/exercice-01.php
+++ exo-02/exercice-01.php
@@ -7,6 +7,6 @@ Ecrire un script PHP qui affiche tous les nombres impairs entre 0 et 150, par or
 croissant : 1 3 5 7...
 */
 
-for ($i=0; $i<151; $i+=2){
+for ($i=1; $i<151; $i+=2){
     echo $i . "\n";
 }
