3b170d1e5312ecb9263dd7ad678f13f9a4d060ea
jolivald <jolivald.jonathan@gmail.com>
2023-10-09 15:51:06 +0200

    exo-02 exercice 2

---
+++ exo-02/exercice-02.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Exercice 2
+1. Écrire un programme qui écrit 500 fois la phrase Je dois faire des
+sauvegardes régulières de mes fichiers.
+*/
+
+for ($i=0; $i<501; $i++){
+    echo "Je dois faire des sauvegardes régulières de mes fichiers.\n";
+}
