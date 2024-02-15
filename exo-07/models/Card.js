3489816b67b2fc9cc27dbf0d8a87cde7535aa950
jolivald <jolivald.jonathan@gmail.com>
2024-02-15 16:49:35 +0100

    add comment for WIP

--- exo-07/models/Card.js
+++ exo-07/models/Card.js
@@ -1,9 +1,13 @@
 import mongoose from 'mongoose';
 import { userSchema } from './User';
 
+// TODO: check mongoose enum for priority, estimate & size
 export const cardSchema = new mongoose.Schema({
   title: String,
   content: String,
+  priority: Number,
+  estimate: Number,
+  size: Number,
   assigned: [userSchema]
 }, { timestamps: true });
 
